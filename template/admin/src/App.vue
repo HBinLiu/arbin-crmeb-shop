3bf6028aee54fe6ae01e57143623fc4d3ed95a8a
From-wh <from-wh@hotmail.com>
2025-03-31 10:06:25 +0800

    refactor: 优化配置文件和样式代码
    - 移除App.vue中未使用的CSS动画代码
    - 重构vue.config.js，简化配置逻辑并优化生产环境构建

--- template/admin/src/App.vue
+++ template/admin/src/App.vue
@@ -133,32 +133,6 @@ body {
   height: 100%;
   font-family: PingFang SC, Arial, Microsoft YaHei, sans-serif;
 }
-// .dialog-fade-enter-active {
-//   animation: anim-open 0.3s;
-// }
-// .dialog-fade-leave-active {
-//   animation: anim-close 0.3s;
-// }
-// @keyframes anim-open {
-//   0% {
-//     transform: translate3d(100%, 0, 0);
-//     opacity: 0;
-//   }
-//   100% {
-//     transform: translate3d(0, 0, 0);
-//     opacity: 1;
-//   }
-// }
-// @keyframes anim-close {
-//   0% {
-//     transform: translate3d(0, 0, 0);
-//     opacity: 1;
-//   }
-//   100% {
-//     transform: translate3d(100%, 0, 0);
-//     opacity: 0;
-//   }
-// }
 .ivu-modal-wrap ::v-deep .connect_customerServer_img {
   display: none;
 }
