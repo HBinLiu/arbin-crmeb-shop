3437415023f8570bac8f62af2a5dc4fd0cfc819b
wuhaotian <442384644@qq.com>
2025-05-27 14:24:42 +0800

    【程序目录】更新本地上传验证

--- crmeb/crmeb/services/upload/storage/Local.php
+++ crmeb/crmeb/services/upload/storage/Local.php
@@ -125,14 +125,16 @@ class Local extends BaseUpload
             if (!in_array($fileHandle->getOriginalMime(), $this->validate['fileMime'])) {
                 return $this->setError('不合法的文件类型');
             }
-            $stream = fopen($fileHandle->getPathname(), 'r');
-            $content = (fread($stream, filesize($fileHandle->getPathname())));
-            if (is_resource($stream)) {
-                fclose($stream);
-            }
-            $image = @imagecreatefromstring($content);
-            if ($image === false) {
-                return $this->setError('文件内容不合法');
+            if (in_array($fileHandle->getOriginalMime(), ['image/x-icon', 'image/png', 'image/gif', 'image/jpeg', 'image/jpg'])) {
+                $stream = fopen($fileHandle->getPathname(), 'r');
+                $content = (fread($stream, filesize($fileHandle->getPathname())));
+                if (is_resource($stream)) {
+                    fclose($stream);
+                }
+                $image = @imagecreatefromstring($content);
+                if ($image === false) {
+                    return $this->setError('文件内容不合法');
+                }
             }
         }
         if ($realName) {
